d17191ed727e4d92d54c910524f180e5933e2e07
MY-Swich <937127460@qq.com>
2020-04-08 21:44:25 +0800

    情感分析

---
+++ sentiment.py
@@ -0,0 +1,20 @@
+import torch
+from torchtext import data
+from torchtext import datasets
+
+SEED = 1234
+
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+TEXT = data.Field(tokenize='spacy')
+LABEL = data.LabelField(dtype=torch.float)
+
+train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
+
+print(f'Number of training examples: {len(train_data)}')
+print(f'Number of testing examples: {len(test_data)}')
+
+# TODO
+# 安装包
